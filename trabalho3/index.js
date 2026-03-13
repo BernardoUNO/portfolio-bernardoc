5c618b0faf2e9269879e0896ad73b9a9bc986ed1
BernardoUNO <169105010+BernardoUNO@users.noreply.github.com>
2026-03-13 19:38:28 -0300

    Update index.js

--- trabalho3/index.js
+++ trabalho3/index.js
@@ -1,1 +1,2 @@
-
+responsabilidade, ser responsivo até agora é para ter uma tela de sobre e outra de depoimentos ou comentarios onde mudando na parte superior alterna entre as telas
+e fazer funcionar em celulares
